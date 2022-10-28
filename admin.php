fae56b4be785d1dede1eef2a502015de53687c20
mayados <mariannedos.cours@gmail.com>
2022-10-28 10:00:06 +0200

    creation page admin php with form

---
+++ admin.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin</title>
+</head>
+<body>
+<div id="container-page">
+    <?php
+
+    /* Il faut avoir accès au tableau $_SESSION pour connaître le nombre de produits actuels */
+    session_start();
+    // require('functions.php');
+    // showMessage();
+    ?>
+    <h1>Ajouter un nouveau menu dans la base de données</h1>
+        <section id="formulaire">
+            <form action="traitement.php?action=ajouterProduitBdd" method="post">
+                <p>
+                    <label>
+                        <p class="label-p">Jour de la semaine :</p>
+                        <!-- required pour indiquer que c'est un champ qui doit obligatoirement être rempli -->
+                        <input required type="text" name="jour">
+                    </label>
+                </p>
+                <p>
+                    <label>
+                        <p class="label-p">Entree :</p>
+                        <input required type="text" name="entree">
+                    </label>
+                </p>
+                <p>
+                    <label>
+                        <p class="label-p">Plat :</p>
+                        <input required type="text" name="plat">
+                    </label>
+                </p>
+                <p>
+                    <label>
+                        <p class="label-p">Dessert :</p>
+                        <input required type="text" name="dessert">
+                    </label>
+                </p>
+                <p>
+                    <input id="ajouter" type="submit" name="submit" value="Ajouter le menu">
+                </p>
+            </form>
+        </section>
+
+        <a id="recap" href="recap.php">Retour accueil restaurant</a>            
+        
+
+    </div>
+</body>
+</html>
