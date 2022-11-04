fb9c18b7715042bd921d9fe2992628590ef4d300
mayados <mariannedos.cours@gmail.com>
2022-11-04 18:51:10 +0100

    change name id and add class

--- admin.php
+++ admin.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/admin.css">
     <title>Admin</title>
 </head>
 <body>
@@ -16,40 +17,40 @@
     // showMessage();
     ?>
     <h1>Changer menu dans la base de données</h1>
-        <section id="formulaire">
-            <form action="traitement.php?action=ajouterMenu" method="post">
+    <section id="formulaire">
+         <form action="traitement.php?action=ajouterMenu" method="post">
                 <p>
-                    <label>
-                        <p class="label-p">Jour de la semaine :</p>
-                        <!-- required pour indiquer que c'est un champ qui doit obligatoirement être rempli -->
-                        <input required type="text" name="jour">
+                <label>
+                    <p class="label-p">Jour de la semaine :</p>
+                    <!-- required pour indiquer que c'est un champ qui doit obligatoirement être rempli -->
+                     <input class="form-champ" required type="text" name="jour">
                     </label>
-                </p>
-                <p>
-                    <label>
-                        <p class="label-p">Entree :</p>
-                        <input required type="text" name="entree">
-                    </label>
-                </p>
-                <p>
-                    <label>
-                        <p class="label-p">Plat :</p>
-                        <input required type="text" name="plat">
-                    </label>
-                </p>
-                <p>
-                    <label>
-                        <p class="label-p">Dessert :</p>
-                        <input required type="text" name="dessert">
-                    </label>
-                </p>
-                <p>
-                    <input id="ajouter" type="submit" name="submit" value="Changer le menu">
-                </p>
-            </form>
-        </section>
+            </p>
+            <p>
+                <label>
+                    <p class="label-p">Entree :</p>
+                    <input class="form-champ" required type="text" name="entree">
+                </label>
+            </p>
+            <p>
+                <label>
+                    <p class="label-p">Plat :</p>
+                    <input class="form-champ" required type="text" name="plat">
+                </label>
+            </p>
+            <p>
+                <label>
+                    <p class="label-p">Dessert :</p>
+                    <input class="form-champ" required type="text" name="dessert">
+                </label>
+            </p>
+            <p>
+                <input id="ajouter" type="submit" name="submit" value="Changer le menu">
+            </p>
+        </form>
+     </section>
 
-        <a id="recap" href="recap.php">Retour accueil restaurant</a>            
+    <a id="recap" href="index.php">Retour accueil restaurant</a>            
         
 
     </div>
